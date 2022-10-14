7ad240d27d88564f18670bd4031d28ebdb9703e3
Ram81 <ramramrakhya81@gmail.com>
2022-10-14 16:39:22 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+
+
+import gzip
+
+from tqdm import tqdm
+
+import prior
+
+try:
+    from prior import LazyJsonDataset
+except:
+    raise ImportError("Please update the prior package (pip install --upgrade prior).")
+
+
+def load_dataset() -> prior.DatasetDict:
+    """Load the houses dataset."""
+    data = {}
+    for split, size in [("train", 10_000), ("val", 1_000), ("test", 1_000)]:
+        with gzip.open(f"{split}.jsonl.gz", "r") as f:
+            houses = [line for line in tqdm(f, total=size, desc=f"Loading {split}")]
+        data[split] = LazyJsonDataset(
+            data=houses, dataset="procthor-dataset", split=split
+        )
+    return prior.DatasetDict(**data)
