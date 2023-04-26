8dc9dff9fe41a3c916f1c7c4920ec8f351d0df9c
roseh-ai2 <roseh@allenai.org>
2023-04-26 14:38:49 -0700

    oops

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def load_dataset() -> prior.DatasetDict:
     #     with gzip.open(f"dataset_files/procthor100k_balanced_partitionedassets_{split}.gzip", "r") as f:
     
     for split, size in zip(("train","val","test"),(20, 20, 20)):
-        with gzip.open(f"dataset_files/{split}_tiny.gzip", "r") as f:
+        with gzip.open(f"{split}_tiny.gzip", "r") as f:
             houses = [line for line in tqdm(f, total=size, desc=f"Loading {split}")]
         data[split] = LazyJsonDataset(
             data=houses, dataset="procthor-100k", split=split
