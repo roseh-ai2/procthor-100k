512306b096b3fabf05185c92a1a1f1dc657e1fb4
roseh-ai2 <roseh@allenai.org>
2023-05-03 14:10:25 -0700

    fixed single version may 3rd, balanced rooms and partitioned assets

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-
-
 import gzip
-
+import os
 from tqdm import tqdm
-
 import prior
+import urllib.request
 
 try:
-    from prior import NoCacheLazyJsonDataset
+    from prior import LazyJsonDataset
 except:
     raise ImportError("Please update the prior package (pip install --upgrade prior).")
 
@@ -15,10 +13,13 @@ except:
 def load_dataset() -> prior.DatasetDict:
     """Load the houses dataset."""
     data = {}
-    for split, size in [("train", 10_000)]:
-        with gzip.open(f"{split}.jsonl.gz", "r") as f:
+    for split, size in zip(("train", "val", "test"), (100_000, 1_000, 1_000)):
+        if not f"procthor100k_balanced_{split}.jsonl.gz" in os.listdir("./"):
+            url = f"https://prior-datasets.s3.us-east-2.amazonaws.com/ilearn-dataset/procthor_100k_balanced_{split}.jsonl.gz"
+            urllib.request.urlretrieve(
+                url, "./procthor_100k_balanced_{}.jsonl.gz".format(split)
+            )
+        with gzip.open(f"procthor_100k_balanced_{split}.jsonl.gz", "r") as f:
             houses = [line for line in tqdm(f, total=size, desc=f"Loading {split}")]
-        data[split] = NoCacheLazyJsonDataset(
-            data=houses, dataset="procthor-10k-uniform", split=split
-        )
+        data[split] = LazyJsonDataset(data=houses, dataset="procthor-100k", split=split)
     return prior.DatasetDict(**data)
