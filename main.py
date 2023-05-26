b7660d750f7e4e674879d9af3ea17e6a01880453
roseh-ai2 <roseh@allenai.org>
2023-05-25 17:09:33 -0700

    tiny set of new houses for eng work

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import gzip
-import os
 from tqdm import tqdm
 import prior
-import urllib.request
 
 try:
     from prior import LazyJsonDataset
@@ -13,13 +11,8 @@ except:
 def load_dataset() -> prior.DatasetDict:
     """Load the houses dataset."""
     data = {}
-    for split, size in zip(("train", "val", "test"), (100_000, 1_000, 1_000)):
-        if not f"procthor_100k_balanced_{split}.jsonl.gz" in os.listdir("./"):
-            url = f"https://prior-datasets.s3.us-east-2.amazonaws.com/ilearn-dataset/procthor_100k_balanced_{split}.jsonl.gz"
-            urllib.request.urlretrieve(
-                url, "./procthor_100k_balanced_{}.jsonl.gz".format(split)
-            )
-        with gzip.open(f"procthor_100k_balanced_{split}.jsonl.gz", "r") as f:
+    for split, size in zip(("train", "val", "test"), (100, 100, 100)):
+        with gzip.open(f"{split}.jsonl.gz", "r") as f:
             houses = [line for line in tqdm(f, total=size, desc=f"Loading {split}")]
         data[split] = LazyJsonDataset(data=houses, dataset="procthor-100k", split=split)
     return prior.DatasetDict(**data)
